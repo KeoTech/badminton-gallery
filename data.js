560353a28ff90643b736c336f58e463cbaeffbf0
KeoTech <chinokeonie.santos@gmail.com>
2025-11-28 03:15:10 +0800

    Added files such as html, js and images

---
+++ data.js
@@ -0,0 +1,72 @@
+var players = [
+  {
+    "Name": "Lee Chong Wei",
+    "About": "Malaysian former professional badminton player",
+    "Born": "October 21, 1982 (age 43 years), Bagan Serai, Malaysia",
+    "picture": "images/lcw.png",
+    "link": "https://bwfbadminton.com/player/50152/lee-chong-wei"
+  },
+  {
+    "Name": "Lin Dan",
+    "About": "Chinese former professional badminton player",
+    "Born": "October 14, 1983 (age 42 years), Longyan, China",
+    "picture": "images/ld.png",
+    "link": "https://bwfbadminton.com/player/50906/lin-dan"
+  },
+  {
+    "Name": "Viktor Axelsen",
+    "About": "Danish professional badminton player",
+    "Born": "January 4, 1994 (age 31 years), Odense, Denmark",
+    "picture": "images/va.png",
+    "link": "https://bwfbadminton.com/player/25831/viktor-axelsen"
+  },
+  {
+    "Name": "Kento Momota",
+    "About": "Japanese former professional badminton player",
+    "Born": "September 1, 1994 (age 31 years), Mino, Kagawa",
+    "picture": "images/km.png",
+    "link": "https://bwfbadminton.com/player/89785/kento-momota"
+  },
+  {
+    "Name": "Loh Kean Yew",
+    "About": "Singaporean professional badminton player",
+    "Born": "June 26, 1997 (age 28 years), Penang, Malaysia",
+    "picture": "images/lky.png",
+    "link": "https://bwfbadminton.com/player/76115/loh-kean-yew"
+  },
+  {
+    "Name": "Anthony Sinisuka Ginting",
+    "About": "Indonesian professional badminton player",
+    "Born": "October 20, 1996 (age 29 years), Cimahi, Indonesia",
+    "picture": "images/asg.png",
+    "link": "https://bwfbadminton.com/player/95661/anthony-sinisuka-ginting"
+  },
+  {
+    "Name": "Chou Tien Chen",
+    "About": "Taiwanese professional badminton player",
+    "Born": "January 8, 1990 (age 35 years), Taipei City, Taipei, Taiwan",
+    "picture": "images/ctc.png",
+    "link": "https://bwfbadminton.com/player/34810/chou-tien-chen"
+  },
+  {
+    "Name": "Shi Yu Qi",
+    "About": "Chinese professional badminton player",
+    "Born": "February 28, 1996 (age 29 years), Nantong, China",
+    "picture": "images/syq.png",
+    "link": "https://bwfbadminton.com/player/57945/shi-yu-qi"
+  },
+  {
+    "Name": "Kevin Sanjaya Sukamuljo",
+    "About": "Indonesian former professional badminton player",
+    "Born": "August 2, 1995 (age 30 years), Banyuwangi, Indonesia",
+    "picture": "images/kss.png",
+    "link": "https://bwfbadminton.com/player/80057/kevin-sanjaya-sukamuljo"
+  },
+  {
+    "Name": "Marcus Fernaldi Gideon",
+    "About": "Indonesian former professional badminton player",
+    "Born": "March 9, 1991 (age 34 years), Jakarta, Indonesia",
+    "picture": "images/mfg.png",
+    "link": "https://bwfbadminton.com/player/26394/marcus-fernaldi-gideon"
+  }
+];
